caaed1af3251c64464a016a32019b2fcee4753ab
Caleb-Irwin <56744087+Caleb-Irwin@users.noreply.github.com>
2021-11-06 12:08:01 -0600

    move api key into env (dotenv)

--- src/routes/search/_api.ts
+++ src/routes/search/_api.ts
@@ -1,8 +1,9 @@
 import type { Response } from '@sveltejs/kit';
+import 'dotenv/config';
 
 export const getData = async (text = '', id = '', length = 250): Promise<Response> => {
   const res = await fetch(
-    `https://ws1.postescanada-canadapost.ca/Capture/Interactive/Find/v1.00/json3ex.ws?Key=TF36-KU97-AB94-ZC85&Text=${text}&Container=${id}&Limit=${length}&Language=en`
+    `https://ws1.postescanada-canadapost.ca/Capture/Interactive/Find/v1.00/json3ex.ws?Key=${process.env['KEY']}&Text=${text}&Container=${id}&Limit=${length}&Language=en`
   );
   const items = await res.json();
   if (!Array.isArray(items?.Items)) {
